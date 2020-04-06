74e1cde24280eda787d3ac24b57f2b41ee162dd4
Mario Dujić <liberasoftware@gmail.com>
2020-04-06 19:21:26 +0200

    added tests to OwnerViewModelTest

--- app/src/test/java/com/groundzero/github/OwnerViewModelTest.kt
+++ app/src/test/java/com/groundzero/github/OwnerViewModelTest.kt
@@ -1,14 +1,19 @@
 package com.groundzero.github
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.groundzero.github.data.Result
+import com.groundzero.github.feature.content.owner.data.Owner
 import com.groundzero.github.feature.content.owner.data.OwnerRepository
 import com.groundzero.github.feature.content.owner.ui.OwnerViewModel
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.times
 import org.mockito.junit.MockitoJUnitRunner
 
@@ -33,7 +38,16 @@ class OwnerViewModelTest {
         Mockito.verify(repository, times(1)).getOwner(USERNAME)
     }
 
+    @Test
+    fun `live data returning correct value`() {
+        val ownerLiveData = MutableLiveData<Result<Owner>>()
+        ownerLiveData.value = result
+        `when`(repository.getOwner(USERNAME)).thenReturn(ownerLiveData)
+        assertEquals(viewModel.getOwner(USERNAME).value, result)
+    }
+
     companion object {
         const val USERNAME = "John"
+        private val result = Result.success(Owner(0))
     }
 }
